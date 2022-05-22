7ac176c7e12c117cc3891f8c826b7654c66127d6
Julián de Pablo <jdptdev@gmail.com>
2022-05-22 16:50:36 +0200

    implemented logo error handling

--- server/services/services.js
+++ server/services/services.js
@@ -4,28 +4,32 @@ const sha256 = require('sha256')
 const jwt = require('jsonwebtoken')
 require('../database/keys')
 
-const timestamp = new Date();
-const timestampAdd = new Date(timestamp.getDate() + 1)
-
 async function getDomainLogo(url) {
   let logo
 
   return new Promise((resolve, reject) => {
-    websiteLogo(url, function (error, result) {
-      if (error) reject('Logo not generated: \n%s'.red, error)
-
-      if (result.icon !== null) {
-        logo = result.icon.href
-        resolve(logo)
-      }
-      else if (result.openGraph[0] !== undefined) {
-        logo = result.openGraph[0].href
-        resolve(logo)
-      }
-      else {
-        reject('Logo not generated: \n%s'.red, error)
-      }
-    })
+    try {
+      websiteLogo(url, function (error, result) {
+        if (result.icon !== null) {
+          logo = result.icon.href
+          resolve(logo)
+        }
+        else if (result.openGraph[0] !== undefined) {
+          logo = result.openGraph[0].href
+          resolve(logo)
+        }
+        else {
+          console.error('Logo not generated: \n%s'.red, error)
+          logo = "/icons/noimage.svg"
+          resolve(logo)
+        }
+      })
+    }
+    catch (error) {
+      console.error('Logo not generated: \n%s'.red, error)
+      logo = "/icons/noimage.svg"
+      resolve(logo)
+    }
   })
 }
 
